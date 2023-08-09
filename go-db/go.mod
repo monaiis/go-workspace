c7a0b0ee32f561988ecce1fa23be6809c5c7f5ed
monaiis <90849453+monaiis@users.noreply.github.com>
2023-08-09 17:27:17 +0700

    updated

--- go-db/go.mod
+++ go-db/go.mod
@@ -2,9 +2,12 @@ module go-db
 
 go 1.20
 
+require github.com/microsoft/go-mssqldb v1.5.0
+
 require (
 	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
-	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/crypto v0.9.0 // indirect
 )
--- go-db/main.go
+++ go-db/main.go
@@ -1,35 +1,38 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
+	"log"
 
-	_ "github.com/denisenkom/go-mssqldb"
+	_ "github.com/microsoft/go-mssqldb"
 )
 
+var server = "mysqlserver08082023.database.windows.net"
+var port = 1433
+var user = "azureuser"
+var password = "p@ssw0rd"
+var database = "mySampleDatabase"
+
 func main() {
-	db, err := sql.Open("sqlserver", "sqlserver://sa:P@ssw0rd@13.76.163.73/?database=techcoach")
-	if err != nil {
-		panic(err)
-	}
+	// Build connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	query := "SELECT * FROM cover"
-	rows, err := db.Query(query)
+	// Create connection pool
+	db, err := sql.Open("sqlserver", connString)
 
 	if err != nil {
-		panic(err)
+		log.Fatal("Error creating connection pool: ", err.Error())
 	}
 
-	id := 0
-	name := ""
-	ok := rows.Next()
-	if ok {
-		rows.Scan(&id, &name)
+	ctx := context.Background()
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
-	println(id, name)
+	fmt.Printf("Successfully connected")
 }
